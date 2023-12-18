e04eb8da6bda69c7de659cbbe88aa46c59ed7f7e
sunandasharma61 <sunandasharma60101@gmail.com>
2023-12-18 10:16:10 +0530

    updated

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_private_link_service" "private_link_service" {
       name                       = nat_ip_configuration.value.name
       private_ip_address         = nat_ip_configuration.value.private_ip_address
       private_ip_address_version = nat_ip_configuration.value.private_ip_address_version
-     // subnet_id                  = flatten([for subnet in var.subnet_output : subnet.id if contains(each.value.subnet_name, subnet.name) == true]) # subnet_id = element([for subnet in var.subnet_output : subnet.id if subnet.name == each.value.subnet_name], 0)
+      subnet_id                  = var.subnet_output[each.value.subnet_name].id
       primary                    = true
     }
   }
