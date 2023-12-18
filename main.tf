6f712726747bec6761b6d23e1e3c484cc74c666c
sunandasharma61 <sunandasharma60101@gmail.com>
2023-12-18 12:23:51 +0530

    updated

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_private_link_service" "private_link_service" {
       name                       = nat_ip_configuration.value.name
       private_ip_address         = nat_ip_configuration.value.private_ip_address
       private_ip_address_version = nat_ip_configuration.value.private_ip_address_version
-      subnet_id                  = lookup(var.subnet_output, format("%s", nat_ip_configuration.value.subnet_name)).id
+      subnet_id                  = lookup(var.subnet_output, format("%s", nat_ip_configuration.value.subnet_name),0).id
       primary                    = true
     }
   }
