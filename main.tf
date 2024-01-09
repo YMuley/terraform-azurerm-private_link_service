c705654493a862ae567b2e381b4cb9b0efe5bd98
sunandasharma61 <sunandasharma60101@gmail.com>
2024-01-09 18:37:08 +0530

    config

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "azurerm_private_link_service" "private_link_service" {
-  for_each            = local.private_link_service
-  name                = each.value.name
-  location            = each.value.location
-  resource_group_name = var.resource_group_output[each.value.resource_group_name].name
-  load_balancer_frontend_ip_configuration_ids = flatten([for load_balancer in var.load_balancer_output : load_balancer.id if contains(each.value.load_balancer_name, load_balancer.name) == true])
+  for_each                                    = local.private_link_service
+  name                                        = each.value.name
+  location                                    = each.value.location
+  resource_group_name                         = var.resource_group_output[each.value.resource_group_name].name
+  load_balancer_frontend_ip_configuration_ids = each.value.frontend_ip_configuration_ids //flatten([for load_balancer in var.load_balancer_output : load_balancer.id if contains(each.value.load_balancer_name, load_balancer.name) == true])
   tags                                        = each.value.tags == null ? var.default_values.tags : each.value.tags
 
   dynamic "nat_ip_configuration" {
